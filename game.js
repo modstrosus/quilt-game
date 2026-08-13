6675d7e6ee900ddc6af23e6542cb38399669c90e
modstrosus <madisonhynes1996@gmail.com>
2026-08-13 14:32:37 -0230

    added start button

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 console.log("game.js is running");
 
+
+
 function showScreen(name){
 
     document.querySelectorAll("section").forEach(s => s.classList.remove("active"))
@@ -7,4 +9,10 @@ function showScreen(name){
     let selector = "#" + name + "-screen";
 
     document.querySelector(selector).classList.add("active");
+
+    
 }
+
+document.querySelector("#start-btn").addEventListener("click", () => {
+  showScreen("cutting");
+});
